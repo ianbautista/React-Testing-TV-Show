2d762dbf5c0d1369d76fd3a61a6cf877a678c898
ianbautista <ianfbautista@gmail.com>
2020-08-17 12:20:17 -0700

    moved fetchshow

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,9 +1,7 @@
 import axios from "axios";
+
 export const fetchShow = () => {
-	axios
+	return axios
 		.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-		.then((res) => {
-			setShow(res.data);
-			setSeasons(formatSeasons(res.data._embedded.episodes));
-		});
+		.then((res) => res.data);
 };
